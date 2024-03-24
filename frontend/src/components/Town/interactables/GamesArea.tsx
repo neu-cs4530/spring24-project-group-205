11ec0a5a736e9c133ee0ecf723ef98c6a971bcd9
chapinalf <alf.c@northeastern.edu>
2024-03-24 14:53:09 -0400

    scavenger hunt hub UI complete

--- frontend/src/components/Town/interactables/GamesArea.tsx
+++ frontend/src/components/Town/interactables/GamesArea.tsx
@@ -63,65 +63,69 @@ function GameArea({ interactableID }: { interactableID: InteractableID }): JSX.E
   }, [townController, gameAreaController]);
   return (
     <>
-      <Accordion allowToggle>
-        <AccordionItem>
-          <Heading as='h3'>
-            <AccordionButton>
-              <Box flex='1' textAlign='left'>
-                Leaderboard
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-            <AccordionPanel>
-              <Leaderboard results={history} />
-            </AccordionPanel>
-          </Heading>
-        </AccordionItem>
-        <AccordionItem>
-          <Heading as='h3'>
-            <AccordionButton>
-              <Box as='span' flex='1' textAlign='left'>
-                Current Observers
-                <AccordionIcon />
-              </Box>
-            </AccordionButton>
-          </Heading>
-          <AccordionPanel>
-            <List aria-label='list of observers in the game'>
-              {observers.map(player => {
-                return <ListItem key={player.id}>{player.userName}</ListItem>;
-              })}
-            </List>
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
-      <Flex>
-        <Box>
-          {gameAreaController.toInteractableAreaModel().type === 'ConnectFourArea' ? (
-            <ConnectFourArea interactableID={interactableID} />
-          ) : gameAreaController.toInteractableAreaModel().type === 'TicTacToeArea' ? (
-            <TicTacToeArea interactableID={interactableID} />
-          ) : gameAreaController.toInteractableAreaModel().type === 'ScavengerHuntArea' ? (
-            <ScavengerHuntArea interactableID={interactableID} />
-          ) : (
-            <>{INVALID_GAME_AREA_TYPE_MESSAGE}</>
-          )}
-        </Box>
-        <Box
-          style={{
-            height: '400px',
-            overflowY: 'scroll',
-          }}>
-          <div
-            style={{
-              height: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-            }}>
-            <ChatChannel interactableID={gameAreaController.id} />
-          </div>
-        </Box>
-      </Flex>
+      {gameAreaController.toInteractableAreaModel().type === 'ScavengerHuntArea' ? (
+        <ScavengerHuntArea interactableID={interactableID} />
+      ) : (
+        <>
+          <Accordion allowToggle>
+            <AccordionItem>
+              <Heading as='h3'>
+                <AccordionButton>
+                  <Box flex='1' textAlign='left'>
+                    Leaderboard
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+                <AccordionPanel>
+                  <Leaderboard results={history} />
+                </AccordionPanel>
+              </Heading>
+            </AccordionItem>
+            <AccordionItem>
+              <Heading as='h3'>
+                <AccordionButton>
+                  <Box as='span' flex='1' textAlign='left'>
+                    Current Observers
+                    <AccordionIcon />
+                  </Box>
+                </AccordionButton>
+              </Heading>
+              <AccordionPanel>
+                <List aria-label='list of observers in the game'>
+                  {observers.map(player => {
+                    return <ListItem key={player.id}>{player.userName}</ListItem>;
+                  })}
+                </List>
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+          <Flex>
+            <Box>
+              {gameAreaController.toInteractableAreaModel().type === 'ConnectFourArea' ? (
+                <ConnectFourArea interactableID={interactableID} />
+              ) : gameAreaController.toInteractableAreaModel().type === 'TicTacToeArea' ? (
+                <TicTacToeArea interactableID={interactableID} />
+              ) : (
+                <>{INVALID_GAME_AREA_TYPE_MESSAGE}</>
+              )}
+            </Box>
+            <Box
+              style={{
+                height: '400px',
+                overflowY: 'scroll',
+              }}>
+              <div
+                style={{
+                  height: '100%',
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}>
+                <ChatChannel interactableID={gameAreaController.id} />
+              </div>
+            </Box>
+          </Flex>
+        </>
+      )}
     </>
   );
 }
--- frontend/src/components/Town/interactables/ScavengerHunt/ScavengerHuntArea.tsx
+++ frontend/src/components/Town/interactables/ScavengerHunt/ScavengerHuntArea.tsx
@@ -1,11 +1,11 @@
-import { Container, List, ListItem, useToast } from '@chakra-ui/react';
-import React, { useEffect } from 'react';
+import { Button, Container, Flex, Heading, List, ListItem, useToast } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
 import ScavengerHuntAreaController from '../../../../classes/interactable/ScavengerHuntAreaController';
 import { useInteractableAreaController } from '../../../../classes/TownController';
 import useTownController from '../../../../hooks/useTownController';
 import { InteractableID } from '../../../../types/CoveyTownSocket';
 
-export default function TicTacToeArea({
+export default function ScavengerHuntArea({
   interactableID,
 }: {
   interactableID: InteractableID;
@@ -15,14 +15,127 @@ export default function TicTacToeArea({
   const townController = useTownController();
   const toast = useToast();
 
+  // Placeholder data for the leaderboard
+  const leaderboardData = [
+    { username: 'Player1', time: 100 },
+    { username: 'Player2', time: 90 },
+    { username: 'Player3', time: 80 },
+    { username: 'Player4', time: 70 },
+    { username: 'Player5', time: 60 },
+  ];
+
+  const [selectedOption, setSelectedOption] = useState('timed');
+
+  const handleOptionChange = (event: { target: { value: React.SetStateAction<string> } }) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const [selectedOptionTheme, setSelectedOptionTheme] = useState('fruit');
+
+  const handleOptionChangeTheme = (event: { target: { value: React.SetStateAction<string> } }) => {
+    setSelectedOptionTheme(event.target.value);
+  };
+
   useEffect(() => {}, [townController, gameAreaController, toast]);
 
   return (
-    <Container>
-      <List aria-label='Hi'>
-        <ListItem>X:</ListItem>
-        <ListItem>O:</ListItem>
-      </List>
-    </Container>
+    <>
+      <Flex>
+        <Heading as='h1' style={{ marginRight: '10px', fontSize: '25px', marginBottom: '10px' }}>
+          Game Mode:
+        </Heading>
+        <div className='radio-group'>
+          <label style={{ marginRight: '10px', fontSize: '20px' }}>
+            <input
+              type='radio'
+              name='gameMode'
+              value='timed'
+              checked={selectedOption === 'timed'}
+              onChange={handleOptionChange}
+            />{' '}
+            Timed
+          </label>
+          <label style={{ marginRight: '10px', fontSize: '20px' }}>
+            <input
+              type='radio'
+              name='gameMode'
+              value='relaxed'
+              checked={selectedOption === 'relaxed'}
+              onChange={handleOptionChange}
+            />{' '}
+            Relaxed
+          </label>
+        </div>
+      </Flex>
+      <Heading as='h1' style={{ marginRight: '10px', fontSize: '25px' }}>
+        Leaderboard:
+      </Heading>
+      <table style={{ borderCollapse: 'collapse', width: '100%', marginBottom: '10px' }}>
+        <thead>
+          <tr>
+            <th style={{ width: '33%' }}>Rank</th>
+            <th style={{ width: '33%' }}>Username</th>
+            <th style={{ width: '33%' }}>Time (Seconds)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {leaderboardData.map((player, index) => (
+            <tr key={index} style={{ borderBottom: '1px solid #ddd' }}>
+              <td style={{ textAlign: 'center' }}>{index + 1}</td>
+              <td style={{ textAlign: 'center' }}>{player.username}</td>
+              <td style={{ textAlign: 'center' }}>{player.time}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Flex>
+        <Heading as='h1' style={{ marginRight: '10px', fontSize: '25px', marginBottom: '10px' }}>
+          Theme:
+        </Heading>
+        <div className='radio-group'>
+          <label style={{ marginRight: '10px', fontSize: '20px' }}>
+            <input
+              type='radio'
+              name='theme'
+              value='fruit'
+              checked={selectedOptionTheme === 'fruit'}
+              onChange={handleOptionChangeTheme}
+            />{' '}
+            Fruit
+          </label>
+          <label style={{ marginRight: '10px', fontSize: '20px' }}>
+            <input
+              type='radio'
+              name='theme'
+              value='dessert'
+              checked={selectedOptionTheme === 'dessert'}
+              onChange={handleOptionChangeTheme}
+            />{' '}
+            Dessert
+          </label>
+          <label style={{ marginRight: '10px', fontSize: '20px' }}>
+            <input
+              type='radio'
+              name='theme'
+              value='animals'
+              checked={selectedOptionTheme === 'animals'}
+              onChange={handleOptionChangeTheme}
+            />{' '}
+            Animals
+          </label>
+        </div>
+      </Flex>
+      <Flex alignItems='center'>
+        <Heading as='h1' style={{ marginRight: '10px', fontSize: '25px' }}>
+          Hints:
+        </Heading>
+        <Button>Request Hint</Button>
+      </Flex>
+      <>Your hints will be here. Please begin the game and request a hint if you would like one.</>
+      <Flex>
+        <Button style={{ marginRight: '10px', marginTop: '10px' }}>Start Game</Button>
+        <Button style={{ marginTop: '10px' }}>End Game</Button>
+      </Flex>
+    </>
   );
 }
