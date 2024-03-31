414f7f1ed0ef9da6e625055500350bcf569d2847
hochw <hoch.w@northeastern.edu>
2024-03-31 18:14:06 -0400

    winner test

--- townService/src/town/scavengerHunt/ScavengerHuntTimed.test.ts
+++ townService/src/town/scavengerHunt/ScavengerHuntTimed.test.ts
@@ -39,5 +39,30 @@ describe('ScavengerHunt', () => {
       game.iterateClock();
       expect(game.getTimeLeft()).toBe(119);
     });
+    it('should declare the winner based on most items collected', () => {
+      const player = createPlayerForTesting();
+      const player2 = createPlayerForTesting();
+      const burger = new Item('1234', 'burger', { x: 0, y: 0 }, '');
+      const sushi = new Item('5678', 'sushi', { x: 0, y: 0 }, '');
+      const taco = new Item('1011', 'taco', { x: 0, y: 0 }, '');
+
+      themepack.addItem(burger);
+      themepack.addItem(sushi);
+      themepack.addItem(taco);
+
+      game.join(player);
+      game.join(player2);
+      game.startGame(player);
+      expect(game.state.items.length).toBe(3);
+      game.applyMove({ gameID: '1234', playerID: player.id, move: burger });
+      game.applyMove({ gameID: '1234', playerID: player2.id, move: sushi });
+      game.applyMove({ gameID: '1234', playerID: player.id, move: taco });
+      expect(game.state.status).toBe('OVER');
+      expect(game.getScoreForPlayer(player)).toBe(2);
+      expect(game.state.winner).toBe(player.id);
+      expect(game.getTimeLeft()).toBe(120);
+      game.iterateClock();
+      expect(game.getTimeLeft()).toBe(119);
+    });
   });
 });
