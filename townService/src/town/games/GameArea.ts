a7435e954abdb68560736ec1a5201902044c5dfc
hochw <hoch.w@northeastern.edu>
2024-04-08 15:40:25 -0400

    player stays in game

--- townService/src/town/games/GameArea.ts
+++ townService/src/town/games/GameArea.ts
@@ -47,17 +47,19 @@ export default abstract class GameArea<
   protected abstract getType(): InteractableType;
 
   public remove(player: Player): void {
-    if (this._game) {
-      try {
-        this._game.leave(player);
-      } catch (e) {
-        if ((e as InvalidParametersError).message === PLAYER_NOT_IN_GAME_MESSAGE) {
-          // do nothing
-        } else {
-          throw e;
+    if (this.id !== 'Scavenger Hunt') {
+      if (this._game) {
+        try {
+          this._game.leave(player);
+        } catch (e) {
+          if ((e as InvalidParametersError).message === PLAYER_NOT_IN_GAME_MESSAGE) {
+            // do nothing
+          } else {
+            throw e;
+          }
         }
       }
+      super.remove(player);
     }
-    super.remove(player);
   }
 }
